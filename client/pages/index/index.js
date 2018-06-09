0dc2d20559528470b6e85bc2379836d9074f1055
Alex.Y <alexyu87@outlook.com>
2018-06-10 01:42:03 +0800

    Add index page

---
+++ client/pages/index/index.js
@@ -0,0 +1,38 @@
+Page({
+  data: {
+    imgUrls: [
+      '/images/b1.jpg',
+      '/images/b2.jpg',
+      '/images/b3.jpg'
+    ],
+    selectedList: [{
+      name: "蛋黄酥",
+      url: "/images/s4.png",
+      price: 30
+    },{
+      name: "牛轧糖",
+      url: "/images/s5.png",
+      price: 25
+    },{
+      name: "黑森林蛋糕",
+      url: "/images/s6.png",
+      price: 26
+    },{
+      name: "榴莲班戟",
+      url:"/images/s6.png",
+      price:56
+    },{
+      name: "蛋黄酥",
+      url: "/images/s4.png",
+      price: 30
+    }, {
+      name: "牛轧糖",
+      url: "/images/s5.png",
+      price: 25
+    }],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 3000,
+    duration: 800,
+  }
+})
