00b48f7de41e8d58574c231db5056a2e951ad6f6
y00240703 <yuzhoujun@huawei.com>
2018-06-22 17:34:04 +0800

    add login to app onLoad function

--- client/app.js
+++ client/app.js
@@ -1,9 +1,38 @@
 const qcloud = require('./vendor/wafer2-client-sdk/index')
 const config = require('./config')
+const {showBusy,showSuccess, showModal} = require('./utils/util.js')
 
 App({
   onLaunch: function () {
     qcloud.setLoginUrl(config.service.loginUrl)
+    showBusy('正在登录')
+    const session = qcloud.Session.get()
+
+    if (session) {
+      // 第二次登录
+      // 或者本地已经有登录态
+      // 可使用本函数更新登录态
+      qcloud.loginWithCode({
+        success: res => {
+          showSuccess('登录成功')
+        },
+        fail: err => {
+          console.error(err)
+          showModal('登录错误', err.message)
+        }
+      })
+    } else {
+      // 首次登录
+      qcloud.login({
+        success: res => {
+          showSuccess('登录成功')
+        },
+        fail: err => {
+          console.error(err)
+          showModal('登录错误', err.message)
+        }
+      })
+    }
   },
   onShow: function () {
     console.log('App Show')
--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -53,37 +53,6 @@ Page({
   },
 
   onLoad: function () {
-    let self = this;
-    showBusy('正在登录')
-    //qcloud.clearSession()
-    const session = qcloud.Session.get()
-
-    if (session) {
-      // 第二次登录
-      // 或者本地已经有登录态
-      // 可使用本函数更新登录态
-      qcloud.loginWithCode({
-        success: res => {
-          this.setData({ userInfo: res, logged: true })
-          showSuccess('登录成功')
-        },
-        fail: err => {
-          console.error(err)
-          showModal('登录错误', err.message)
-        }
-      })
-    } else {
-      // 首次登录
-      qcloud.login({
-        success: res => {
-          this.setData({ userInfo: res, logged: true })
-          showSuccess('登录成功')
-        },
-        fail: err => {
-          console.error(err)
-          showModal('登录错误', err.message)
-        }
-      })
-    }
+    
   }
 })
